2d7b75306f2bfdf8d74c133a95c06d0421f58c91
Valdemar José Ceccon Junior <valdemar.ceccon@gmail.com>
2020-03-06 01:02:01 -0300

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, err := w.Write([]byte("<h1>Olá papapapa</h1>"))
+		_, err := w.Write([]byte("<h1>hmmmmmm</h1>"))
 
 		if err != nil {
 			log.Println(err.Error())
